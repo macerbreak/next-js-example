70bac8d65afc6123f7ed50bf16f90c3cd9d121bf
Volodymyr Savko <v.savko@dinamicka.com>
2022-06-01 19:19:28 +0300

    added 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,8 @@
+import {Typography} from "@mui/material";
+
+const NotFoundPage = () =>{
+    return <>
+    <Typography>This is Not Found Page 404</Typography>
+    </>
+}
+export default NotFoundPage
